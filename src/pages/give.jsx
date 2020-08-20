61942df3f60ee83e291781fe6f41cb5cc9cc229d
Sam Malanchuk <malanchuksam@gmail.com>
2020-08-20 13:06:12 -0400

    added info on the donate tab

--- src/pages/give.jsx
+++ src/pages/give.jsx
@@ -1,27 +1,22 @@
 import React from 'react';
-import { Page, Navbar, List, ListItem } from 'framework7-react';
+import { Page, Navbar, BlockTitle, Block } from 'framework7-react';
 
 export default class extends React.Component {
   constructor() {
     super();
-
-    this.state = {
-      products: this.$f7.data.products,
-    }
   }
+
   render() {
     return (
       <Page name="give">
         <Navbar title="Give" />
-        <List>
-          {this.state.products.map((product) => (
-            <ListItem
-              key={product.id}
-              title={product.title}
-              link={`/product/${product.id}/`}
-            />
-          ))}
-        </List>
+        <BlockTitle>Send Money</BlockTitle>
+        <Block strong>
+          <p>You can donate using your credit card through our partnered payment processor. Click below to get started.</p>
+          <p>Or you can send a check Payable to:</p>
+          <p>Grace Baptist Church</p>
+          <p>123 Gracehill Dr. Chicago, IL 60625</p>
+        </Block>
       </Page>
     );
   }
